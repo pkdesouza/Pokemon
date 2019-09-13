9b0a9b6bca758d01260f05b7fd057fb9d58f29aa
Patrick de Souza <patrick.dorbacao@ingresso.com>
2019-09-13 20:15:41 -0300

    Refactoring all project e adding new project UnitTest

---
+++ tests/UnitTests/Model/GenerateModel.cs
@@ -0,0 +1,38 @@
+﻿using Bogus;
+using PokemonAPI.ViewModels;
+using System.Collections.Generic;
+
+namespace NUnitTestPokemon.Model
+{
+    public class GenerateModel
+    {
+        public PokemonViewModel PokemonViewModelValid
+        {
+            get => new Faker<PokemonViewModel>()
+                .StrictMode(true)
+                .RuleFor(o => o.Attack, f => f.Random.Number(1, 999))
+                .RuleFor(o => o.Defense, f => f.Random.Number(1, 999))
+                .RuleFor(o => o.Height, f => f.Random.Number(1, 999).ToString())
+                .RuleFor(o => o.Hp, f => f.Random.Number(1, 999))
+                .RuleFor(o => o.Name, f => f.Person.FirstName)
+                .RuleFor(o => o.Speed, f => f.Random.Number(1, 999))
+                .RuleFor(o => o.Types, f => new List<string> { "normal" })
+                .Generate();
+        }
+
+        public PokemonViewModel PokemonViewModelInvalid
+        {
+            get => new Faker<PokemonViewModel>()
+                .StrictMode(true)
+                .RuleFor(o => o.Attack, f => f.Random.Number(950, 1500))
+                .RuleFor(o => o.Defense, f => f.Random.Number(950, 1500))
+                .RuleFor(o => o.Height, f => f.Random.Number(1, 1500).ToString())
+                .RuleFor(o => o.Hp, f => f.Random.Number(950, 1500))
+                .RuleFor(o => o.Name, f => f.Person.FirstName)
+                .RuleFor(o => o.Speed, f => f.Random.Number(950, 1500))
+                .RuleFor(o => o.Types, f => new List<string> { "normal" })
+                .Generate();
+        }
+    }
+}
+
---
+++ tests/UnitTests/ViewModelTest/PokemonViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnitTestPokemon.Model;
+using Xunit;
+
+namespace NUnitTestPokemon.ViewModelTest
+{
+    public class PokemonViewModelTest
+    {
+        public GenerateModel GenerateModel { get => new GenerateModel(); }
+
+        [Fact]
+        public void Valid() {
+            void valid() => GenerateModel.PokemonViewModelValid.Valid();
+            var ex = Record.Exception(valid);
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void Invalid()
+        {
+            void valid() => GenerateModel.PokemonViewModelInvalid.Valid();
+            var ex = Record.Exception(valid);
+            Assert.NotNull(ex);
+        }
+    }
+}
