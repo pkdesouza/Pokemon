20567efe0469fb03893425f0d7456e5aa491d423
Patrick de Souza <pkdesouza@gmail.com>
2019-10-06 23:08:35 -0300

    addiing new project frontend and files (create and read all pokemons)

---
+++ src/frontend/src/app/pokemon/pokemon-create.component.spec.ts
@@ -0,0 +1,23 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokemonCreateComponent } from './pokemon-create.component';
+
+describe('PokemonCreateComponent', () => {
+  let component: PokemonCreateComponent;
+  let fixture: ComponentFixture<PokemonCreateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PokemonCreateComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PokemonCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
